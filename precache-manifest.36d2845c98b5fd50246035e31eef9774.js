456acd01117e967711d22a2c0779fc5f67497438
sikorskiy <webmaster@rabotaplus.ua>
2019-08-23 14:51:52 +0300

    Updates

---
+++ precache-manifest.36d2845c98b5fd50246035e31eef9774.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5b36607517ebab372383066d92bb1a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "1d01ad2a0a239a7e8dd4",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "13346c6090229f9be257",
+    "url": "/weather-app/static/css/main.79553b6d.chunk.css"
+  },
+  {
+    "revision": "1d01ad2a0a239a7e8dd4",
+    "url": "/weather-app/static/js/2.5c5f3554.chunk.js"
+  },
+  {
+    "revision": "13346c6090229f9be257",
+    "url": "/weather-app/static/js/main.c6549904.chunk.js"
+  },
+  {
+    "revision": "7a390137f1e79ffd409f",
+    "url": "/weather-app/static/js/runtime~main.caa3d5bf.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  }
+]);
