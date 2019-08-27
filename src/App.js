ba3d57cb1a97e66d3f9cf176902c99ba1cdda6f7
sikorskiy <webmaster@rabotaplus.ua>
2019-08-27 09:44:08 +0300

    https fixed v2

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
 
     if(city && country) {
 
+      // https fixed
       const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
       const data = await api_call.json();
 
